1f21ea906f057555c857b4c1bda3e796c701de09
MioBestWaifu <Fenix1603@hotmail.com>
2023-08-30 19:54:28 -0300

    Update BackendApplication.java

--- src/main/java/com/aishow/backend/BackendApplication.java
+++ src/main/java/com/aishow/backend/BackendApplication.java
@@ -70,6 +70,7 @@ public class BackendApplication {
 
 			String sampleData = "Hello World";
 			var x = blobServiceClient.getBlobContainerClient("images").getBlobClient("images/teste.txt").getBlockBlobClient();
+			System.out.println(x.getBlobUrl());
 			try (ByteArrayInputStream dataStream = new ByteArrayInputStream(sampleData.getBytes())) {
         		x.upload(dataStream, sampleData.length());
     		} catch (IOException ex) {
