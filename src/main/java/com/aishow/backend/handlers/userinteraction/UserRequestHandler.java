5c45ff5c2e9418dd15acef2d4c6784813b6625af
MioBestWaifu <Fenix1603@hotmail.com>
2023-10-22 19:15:11 -0300

    terminando mesmo

--- src/main/java/com/aishow/backend/handlers/userinteraction/UserRequestHandler.java
+++ src/main/java/com/aishow/backend/handlers/userinteraction/UserRequestHandler.java
@@ -1,7 +1,13 @@
 package com.aishow.backend.handlers.userinteraction;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import com.aishow.backend.data.DatabaseConnection;
+import com.aishow.backend.data.StatementPreparer;
 import com.aishow.backend.handlers.BaseHandler;
+import com.aishow.backend.models.UserInformation;
+import com.mysql.cj.xdevapi.PreparableStatement;
 
 public class UserRequestHandler extends BaseHandler{
     @Override
@@ -14,8 +20,19 @@ public class UserRequestHandler extends BaseHandler{
      * Passar esse metodo pro normal com o AUTH, passar o id vai ser desnecessario
      * @param params passar id no 0
      */
+
+    //AUTH SECURITY
     @Override
     public <T, G> G handle(T reqBody, String[] params) {
-        return (G) DatabaseConnection.getRequestedUserInformation(Integer.parseInt(params[0]));
-    }          
+        try {
+            return (G) getUserById(Integer.parseInt(params[0]));
+        } catch (SQLException ex){
+            return null;
+        }
+    }
+    
+    public UserInformation getUserById(int id) throws SQLException{
+        PreparedStatement st = StatementPreparer.getUserById(DatabaseConnection.getConnection(), id);
+        return UserInformation.fromResultSet(DatabaseConnection.runQuery(st));
+    }
 }
